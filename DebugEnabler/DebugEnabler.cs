0f58f94ca03e5d4dee5b2737b442e2311020f53b
Yukai Li <yukaili.geek@gmail.com>
2014-06-26 20:40:10 -0600

    Added debug enabler patch

---
+++ DebugEnabler/DebugEnabler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DustAetPatchingPlatform;
+using Dust;
+
+namespace DebugEnabler
+{
+    class DebugEnabler : ILoader
+    {
+        public string Name
+        {
+            get { return "Debug Enabler"; }
+        }
+
+        public int Priority
+        {
+            get { return 1000; }
+        }
+
+        public void Load()
+        {
+            Game1.canDebug = true;
+        }
+    }
+}
