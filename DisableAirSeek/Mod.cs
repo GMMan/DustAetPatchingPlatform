89880e481c19ad7a5d338037fa9021887b2539b3
Yukai Li <yukaili.geek@gmail.com>
2014-06-28 15:13:43 -0600

    Err... that didn't all go through. Let's try it again.

---
+++ DisableAirSeek/Mod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using DustAetPatchingPlatform;
+using NativeAssemblerInjection;
+using Dust.CharClasses;
+
+namespace DisableAirSeek
+{
+    class Mod : ILoader
+    {
+        public string Name
+        {
+            get { return "Disable Aerial Dust Storm Seeking Mod"; }
+        }
+
+        public int Priority
+        {
+            get { return 1000; }
+        }
+
+        public void Load()
+        {
+            MethodUtil.ReplaceMethod(typeof(Mod).GetMethod("SeekBoundsReplacement"), typeof(Character).GetMethod("SeekBounds", BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+
+        public bool SeekBoundsReplacement(Character[] c, int i)
+        {
+            return false;
+        }
+    }
+}
