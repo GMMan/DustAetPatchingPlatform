387da59c1966a4b3a97e0802b0a3e40a80c06e79
Yukai Li <yukaili.geek@gmail.com>
2014-06-29 09:25:20 -0600

    Added language switcher

---
+++ SwitchLang/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Language Switcher")]
+[assembly: AssemblyDescription("Allows game language to be set independently of system locale.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GMWare")]
+[assembly: AssemblyProduct("Dust: AET Patching Platform")]
+[assembly: AssemblyCopyright("Copyright © cyanic 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1845874c-a464-40b2-8192-5138f1ff00de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SwitchLang/SwitchLang.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Globalization;
+using System.Resources;
+using System.Windows.Forms;
+using DustAetPatchingPlatform;
+using NativeAssemblerInjection;
+
+namespace SwitchLang
+{
+    class SwitchLang : ILoader
+    {
+        static readonly string LangOptFileName = "lang.txt";
+
+        public string Name
+        {
+            get { return "Language Switcher"; }
+        }
+
+        public int Priority
+        {
+            get { return 1000; }
+        }
+
+        public void Load()
+        {
+            if (!File.Exists(LangOptFileName)) return;
+            string lang = File.ReadAllText(LangOptFileName).Trim();
+            if (string.IsNullOrWhiteSpace(lang) || lang.ToLower().Contains("system")) return;
+
+            Type myType = GetType();
+
+            try
+            {
+                CultureInfo ci = CultureInfo.GetCultureInfo(lang);
+                CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = ci;
+                MethodUtil.ReplaceMethod(myType.GetMethod("SetResourcesReplacement"), typeof(Dust.Game1).GetMethod("SetResources"));
+
+                foreach (Type t in typeof(Dust.Game1).Assembly.GetTypes().Where((tt) => tt.FullName.StartsWith("Dust.Strings")))
+                {
+                    PropertyInfo cultureProp = t.GetProperty("Culture", BindingFlags.Static | BindingFlags.NonPublic);
+                    cultureProp.SetValue(null, ci, null);
+                }
+            }
+            catch (CultureNotFoundException)
+            {
+                MessageBox.Show("Could not find specified culture. Please check your " + LangOptFileName + " file.", "Dust: AET Language Switcher", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void SetResourcesReplacement()
+        {
+            // Do nothing. We've already set the culture.
+        }
+    }
+}
