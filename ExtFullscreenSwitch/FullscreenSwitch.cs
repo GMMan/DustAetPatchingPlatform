30105b5c63f47eb1b60666dee8e3eccbbcfe9b23
Yukai Li <yukaili.geek@gmail.com>
2014-12-19 20:46:29 -0500

    Added thing to allow setting full screen mode via command line args.

---
+++ ExtFullscreenSwitch/FullscreenSwitch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using System.Windows.Forms;
+using DustAetPatchingPlatform;
+using Microsoft.Xna.Framework;
+using Dust;
+
+namespace ExtFullscreenSwitch
+{
+    public class FullscreenSwitch : ILoader
+    {
+        bool ranOnce = false;
+
+        public string Name
+        {
+            get { return "Command Line Full Screen Switch Implementation"; }
+        }
+
+        public int Priority
+        {
+            get { return 1000; }
+        }
+
+        public void Load()
+        {
+            Platform.RegisterLoadAfterGameInit(parseAndSetFullscreen);
+        }
+
+        void parseAndSetFullscreen()
+        {
+            if (!ranOnce)
+            {
+                while (Game1.storage == null || Game1.storage.storeResult != Dust.Storage.StoreResult.Loaded) Thread.Sleep(50);
+            }
+            else
+            {
+                while (Game1.menu.curMenuPage < 0) Thread.Sleep(50);
+            }
+
+            string[] args = Environment.GetCommandLineArgs();
+            for (int i = 1; i < args.Length; ++i)
+            {
+                string arg = args[i].ToUpperInvariant();
+                bool switched = false;
+                if (arg == "/WINDOW")
+                {
+                    Game1.settings.FullScreen = Game1.graphics.IsFullScreen = false;
+                    switched = true;
+                }
+                else if (arg == "/FULLSCREEN")
+                {
+                    Game1.settings.FullScreen = Game1.graphics.IsFullScreen = true;
+                    switched = true;
+                }
+
+                if (switched)
+                {
+                    // Yank control out of Game1 instance until I can figure out how to find the primary thread
+                    GameWindow window = ((Game)typeof(GraphicsDeviceManager).GetField("game", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Game1.graphics)).Window;
+                    Form form = (Form)window.GetType().GetProperty("Form", BindingFlags.Instance | BindingFlags.NonPublic).GetGetMethod(true).Invoke(window, null);
+                    form.Invoke(new Action(Game1.graphics.ApplyChanges));
+                    break;
+                }
+            }
+
+            // Pressing any key reloads settings for some stupid reason.
+            // They're global. Once you've loaded it, you don't need to do it again.
+            // It's not like it's going to change externally.
+            if (!ranOnce)
+            {
+                ranOnce = true;
+                new Thread(parseAndSetFullscreen).Start();
+            }
+        }
+    }
+}
---
+++ ExtFullscreenSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Command Line Full Screen Switch Implementation")]
+[assembly: AssemblyDescription("Allows switching to full screen or windowed mode using command line arguments.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GMWare")]
+[assembly: AssemblyProduct("Dust: AET Patching Platform")]
+[assembly: AssemblyCopyright("Copyright © cyanic 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09c1b911-622e-4ce9-9db0-cd2e036f1495")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
