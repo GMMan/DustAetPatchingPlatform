e853ba631ce8a97654701f2dd43ce200b9adbd84
Yukai Li <yukaili.geek@gmail.com>
2016-06-19 18:18:48 -0600

    Add DRM-free bootstrapper

---
+++ PartsWrapped/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PartsWrapped")]
+[assembly: AssemblyDescription("Platform bootstrapper for Dust: AET DRM-free version.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GMWare")]
+[assembly: AssemblyProduct("Dust: AET Patching Platform")]
+[assembly: AssemblyCopyright("Copyright © cyanic 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bfe5e4a2-3ae1-434b-86d9-6bc1fa2cfa96")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
